c3eac22ce724774a1ca7ba0405c73835576060f2
Krystian Slawinski <krisq16@interia.pl>
2017-06-29 18:45:08 +0100

    Initial Committ

---
+++ Source/AwesomeRPG/AwesomeRPGGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "AwesomeRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AWESOMERPG_API AAwesomeRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
